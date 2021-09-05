6854d41ea25404bc54d014de0de0bd12609981fc
Burdo Ilya <raf91@inbox.ru>
2021-09-05 21:24:03 +0300

    new quest started

---
+++ src/Kata.java
@@ -0,0 +1,20 @@
+//Complete the solution so that it returns true if the first argument(string) passed in ends with the 2nd argument (also a string).
+//        Examples:
+//        solution('abc', 'bc') // returns true
+//        solution('abc', 'd') // returns false
+
+//import org.junit.Test;
+//import static org.junit.Assert.assertEquals;
+//import org.junit.runners.JUnit4;
+//// TODO: Replace examples and use TDD development by writing your own tests
+//public class SolutionTest {
+//    @Test
+//    public void testSomething() {
+//        // assertEquals("expected", "actual");
+//    }
+//}
+
+
+
+public class Kata {
+}
